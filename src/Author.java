d286ceda899590f2bc4e28b86e3c3432c0789776
jordy muniz <jordy.muniz96@gmail.com>
2021-01-09 12:31:07 -0600

    update

--- src/Author.java
+++ src/Author.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
